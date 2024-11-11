426a09bdd9d11f465ce1d29685f0f0b2ec6d0518
Jangmyun <cndlsrb2739@gmail.com>
2024-11-11 22:45:44 +0900

    add control panel onClick function

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -1,11 +1,30 @@
 import publicStyles from "../styles/public.module.css";
 import controlPanelStyles from "../styles/ControlPanel.module.css";
+import useModalStore from "../store/modalStore";
 
 function ControlPanel() {
+  const { toggleModal, setModalOption } = useModalStore();
+
   return (
     <div className={controlPanelStyles.controlPanel}>
-      <button className={publicStyles.button}>Add</button>
-      <button className={publicStyles.button}>Update</button>
+      <button
+        className={publicStyles.button}
+        onClick={() => {
+          toggleModal();
+          setModalOption(true);
+        }}
+      >
+        Add
+      </button>
+      <button
+        className={publicStyles.button}
+        onClick={() => {
+          toggleModal();
+          setModalOption(false);
+        }}
+      >
+        Update
+      </button>
     </div>
   );
 }
