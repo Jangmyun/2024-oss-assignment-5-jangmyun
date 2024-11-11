619df0b9c0bb56d449ece1c1a62430521ff2efe7
Jangmyun <cndlsrb2739@gmail.com>
2024-11-11 18:33:53 +0900

    add ControlPanel Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import ControlPanel from "./components/ControlPanel";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <ControlPanel></ControlPanel>
     </div>
   );
 }
