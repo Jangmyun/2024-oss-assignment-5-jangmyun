d7dc3b2768d72bca7fcd7b571ddb63f4a4614e0c
Jangmyun <cndlsrb2739@gmail.com>
2024-11-11 22:46:52 +0900

    add modal component

--- src/ShowList.js
+++ src/ShowList.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import ControlPanel from "./components/ControlPanel";
+import Modal from "./components/Modal";
 
 function ShowList() {
   return (
     <div>
       <Header></Header>
       <ControlPanel></ControlPanel>
+      <Modal></Modal>
     </div>
   );
 }
