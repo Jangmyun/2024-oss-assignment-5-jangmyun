9830915e2b4cd254acc33f2c4a300f21ef1091d1
Jangmyun <cndlsrb2739@gmail.com>
2024-11-11 20:05:21 +0900

    modal basic layout

---
+++ src/components/Modal.js
@@ -0,0 +1,12 @@
+import modalStyles from "../styles/Modal.module.css";
+
+function Modal() {
+  return (
+    <div className={modalStyles.overlay}>
+      <div className={modalStyles.container}>
+        <h1>dldldldl</h1>
+        <p>dlkfjsdlfkjsldkfsldkjflsdkjflks</p>
+      </div>
+    </div>
+  );
+}
