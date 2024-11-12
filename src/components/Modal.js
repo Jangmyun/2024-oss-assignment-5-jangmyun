169d1e5f8357ebe29409aee7447a6f461c92b4de
Jangmyun <cndlsrb2739@gmail.com>
2024-11-12 17:50:31 +0900

    modal toggle logic

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,14 +1,28 @@
 import useModalStore from "../store/modalStore";
 import modalStyles from "../styles/Modal.module.css";
+import AddForm from "./forms/AddForm";
+import UpdateForm from "./forms/UpdateForm";
 
 function Modal() {
-  const { isModalOpened, toggleModal } = useModalStore();
+  const { isModalOpened, toggleModal, modalOption } = useModalStore();
 
   if (!isModalOpened) return null;
 
   return (
-    <div className={modalStyles.overlay} onClick={toggleModal}>
-      <div className={modalStyles.container}></div>
+    <div
+      className={modalStyles.overlay}
+      onClick={(e) => {
+        toggleModal();
+      }}
+    >
+      <div
+        className={modalStyles.container}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        {modalOption ? <AddForm /> : <UpdateForm />}
+      </div>
     </div>
   );
 }
