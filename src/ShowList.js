a53796de65eb45bdfd34f0d669c028edeae3ac72
Jangmyun <cndlsrb2739@gmail.com>
2024-11-12 17:51:31 +0900

    add CardContainer Component

--- src/ShowList.js
+++ src/ShowList.js
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import ControlPanel from "./components/ControlPanel";
 import Modal from "./components/Modal";
+import CardContainer from "./components/CardContainer";
 
 function ShowList() {
   return (
     <div>
       <Header></Header>
       <ControlPanel></ControlPanel>
+      <CardContainer></CardContainer>
       <Modal></Modal>
     </div>
   );
